0d27ee1c8bc0488f65c56db8379bf5cba7c9104e
Your Name <you@example.com>
2023-02-04 23:14:19 -0500

    up

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,49 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
+
+import { Canvas } from '@react-three/fiber'
+import * as THREE from 'three'
+import {useRef,useLayoutEffect} from 'react';
+import { Line, Controls } from './Lines3';
+
+
+type props = {
+  start: number[],
+  end: number[]
+}
+// function Line({ start, end }: props) {
+//   const ref = useRef<THREE.Line>()
+//   useLayoutEffect(() => {
+//     if (ref.current){
+//       ref.current.geometry.setFromPoints([start, end].map((point) => new THREE.Vector3(...point)))
+//     }
+//   }, [start, end])
+//   return (
+//     <line onClick={(e) => console.log('click')} ref={ref as any}>
+//       <bufferGeometry />
+//       <lineBasicMaterial color="hotpink" />
+//     </line>
+//   )
+// }
+
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+
+    <div style={{ position: "relative", width: 800, height: 800 }}>
+        <Canvas orthographic={true} camera={{ left: -300, right: 300, top: 300, bottom: -300, near: 0, far: 5}}>
+          <mesh >
+            <ringBufferGeometry args={[10,20,32]} />
+            <meshStandardMaterial />
+            {/* <bufferGeometry position={[]}/> */}
+          </mesh>
+          {/* <Line start={[-100, 0, 0]} end={[100, 300, 0]} /> */}
+          <Controls>
+        <Line defaultStart={[-100, -100, 0]} defaultEnd={[0, 100, 0]} />
+        <Line defaultStart={[0, 100, 0]} defaultEnd={[100, -100, 0]} />
+      </Controls>
+          {/* <lineCurve args={[{[0,0],[100,100]}]}/> */}
+        </Canvas>
     </div>
   );
 }
